938a8107623d3d865e28e1735a05ff71ecc98ce9
Никита Бобков <n.bobkov99@bk.ru>
2022-12-25 16:59:07 +0300

    Готово5

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,34 +1,64 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
+# from core.models import CreatedModel
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Заголовок'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='ЧПУ',
+    )
+    description = models.TextField(
+        max_length=400,
+        verbose_name='Описание'
+    )
 
-    def __str__(self) -> str:
+    class Meta:
+        verbose_name_plural = 'Группы'
+        verbose_name = 'Группу'
+
+    def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(
+        max_length=400,
+        verbose_name='Текст поста',
+        help_text='Текст нового поста'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
+        related_name='posts',
         blank=True,
         null=True,
-        related_name="posts"
+        verbose_name='Группа',
+        help_text='Группа, к которой будет относиться пост'
     )
 
     class Meta:
-        ordering = ('pub_date',)
+        ordering = ['-pub_date']
+        verbose_name_plural = 'Посты'
+        verbose_name = 'Пост'
+
+    def __str__(self):
+        return self.text[:15]
